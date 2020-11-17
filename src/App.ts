cab007da80389a5e08386d06301a4b718caf8f85
Bruno Castanho <bruno.castanho@gamersclub.com.br>
2020-11-17 12:41:26 -0300

    chore: add heroku procfile

--- src/App.ts
+++ src/App.ts
@@ -7,14 +7,11 @@ import { AppModule } from './AppModule'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.useGlobalFilters(
-    new ValidationExceptionFilter(),
-    new NotFoundExceptionFilter()
-  )
+  app.useGlobalFilters(new ValidationExceptionFilter(), new NotFoundExceptionFilter())
 
   app.useGlobalPipes(new ClassValidatorValidationPipe())
 
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
 
 bootstrap()
